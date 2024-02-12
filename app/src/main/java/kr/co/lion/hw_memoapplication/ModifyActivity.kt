f675d51fa88c4bb1030d10f11f7905e43c34a2ef
Jieun <lje2000lje@naver.com>
2024-02-12 16:07:16 +0900

    Style: ModifyActivity 메모 수정 UI 구현

---
+++ app/src/main/java/kr/co/lion/hw_memoapplication/ModifyActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.hw_memoapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify)
+    }
+}
