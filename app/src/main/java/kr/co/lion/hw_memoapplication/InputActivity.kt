09fe21122e24432ce94ee4c2594d5c4cc9e01b85
Jieun <lje2000lje@naver.com>
2024-02-12 16:55:45 +0900

    Feat: InputActivity 툴바 기본 설정 및 유효성 검사, 메모 저장 함수 추가

--- app/src/main/java/kr/co/lion/hw_memoapplication/InputActivity.kt
+++ app/src/main/java/kr/co/lion/hw_memoapplication/InputActivity.kt
@@ -2,10 +2,74 @@ package kr.co.lion.hw_memoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kr.co.lion.hw_memoapplication.databinding.ActivityInputBinding
+import java.time.LocalDate
 
 class InputActivity : AppCompatActivity() {
+    lateinit var binding : ActivityInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
+
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setToolbar()
+    }
+
+    // 툴바 설정
+    fun setToolbar() {
+        binding.apply {
+            inputToolbar.apply {
+                inflateMenu(R.menu.input_menu)
+
+                setNavigationIcon(R.drawable.ic_back)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.menu_item_done -> {
+                            checkInput()
+                        }
+                    }
+                    true
+                }
+            }
+        }
+    }
+
+    // 유효성 검사
+    fun checkInput() {
+        binding.apply {
+            val title = inputTitleEdit.text.toString()
+            if (title.trim().isEmpty()) {
+                Toast.makeText(this@InputActivity,"제목을 입력해주세요", Toast.LENGTH_LONG).show()
+            }
+
+            val content = inputContentEdit.text.toString()
+            if (content.trim().isEmpty()) {
+                Toast.makeText(this@InputActivity, "내용을 입력해주세요", Toast.LENGTH_LONG).show()
+            }
+
+            if(title.isNotEmpty() && content.isNotEmpty()) {
+                addMemoData()
+                finish()
+            }
+        }
+    }
+
+    // 메모 저장
+    fun addMemoData() {
+        binding.apply {
+            val memo = Memo()
+            memo.title = inputTitleEdit.text.toString()
+            memo.content = inputContentEdit.text.toString()
+            memo.date = LocalDate.now()
+
+            Util.memoList.add(memo)
+        }
     }
 }
